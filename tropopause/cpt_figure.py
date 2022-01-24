a7af22d509d00626c64fbaca88c8dddfb4a9adbd
michad1111 <hadwigermichael@gmail.com>
2022-01-24 16:00:49 +0100

    small fixes in figure

--- tropopause/cpt_figure.py
+++ tropopause/cpt_figure.py
@@ -8,19 +8,20 @@ def cpt_fig(data):
         ax[idx].plot(ds.altitude, ds.temperature, label=label)
         alt = ds.cpt_alt.values
         temp = ds.cpt_temp.values
+        temp_delta = ds.temperature.max()-ds.temperature.min()
         ax[idx].annotate(
             f"CPT: \n ({alt:.0f}, {temp:.2f})",
             xy=(alt, temp),
-            xytext=(alt, temp + 15),
+            xytext=(alt, temp + temp_delta*0.25),
             arrowprops=dict(arrowstyle="->", connectionstyle="arc3"),
             horizontalalignment="center",
         )
-        if idx == 0:
+        try:
             ax[idx].set_title(
                 f"latitude {ds.lat_limit.values}, time {ds.time.values.astype('datetime64[M]')}"
             )
-        elif idx == 1:
-            ax[1].set_title(
+        except AttributeError:
+            ax[idx].set_title(
                 f"latitude {ds.lat_limit.values}, time {ds.time_limit.values[0]}"
             )
         ax[idx].set_ylabel("temperature / K")
